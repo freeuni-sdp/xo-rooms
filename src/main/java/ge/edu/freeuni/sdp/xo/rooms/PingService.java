b16028634351790c29720050d272964d2f08c35f
giga-ts <gig.tsiklauri@gmail.com>
2015-05-30 22:10:58 +0400

    ping service added

---
+++ src/main/java/ge/edu/freeuni/sdp/xo/rooms/PingService.java
@@ -0,0 +1,16 @@
+package ge.edu.freeuni.sdp.xo.rooms;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.*;
+
+@Path("ping")
+@Consumes( { MediaType.APPLICATION_JSON})
+@Produces( { MediaType.APPLICATION_JSON})
+public class PingService {
+
+  @GET
+  public Response get() {
+    return Response.ok().build();
+  }
+
+}
