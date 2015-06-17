315bf54baaca2a3923ea310287d7cc07c40e5732
Gio <ggogi12@freeuni.edu.ge>
2015-06-17 15:32:18 +0400

    fixed bug #11

--- src/main/java/ge/edu/freeuni/sdp/xo/rooms/data/FakeRepositoryFactory.java
+++ src/main/java/ge/edu/freeuni/sdp/xo/rooms/data/FakeRepositoryFactory.java
@@ -7,8 +7,8 @@ public class FakeRepositoryFactory {
 		if(repo == null){
 			repo = InMemoryRepository.getInstance();
 		
-			Room empty = new Room(1, -1, -1);
-			Room one = new Room(2, 1, -1);
+			Room empty = new Room(1, null, null);
+			Room one = new Room(2, 1, null);
 			Room full = new Room(3, 1, 2);
 			
 			repo.insertOrUpdate(full);
