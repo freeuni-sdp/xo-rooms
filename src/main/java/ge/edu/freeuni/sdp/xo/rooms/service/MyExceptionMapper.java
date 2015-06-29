acd8505a6b66fe361370f721e59f813485da0c58
Gio <ggogi12@freeuni.edu.ge>
2015-06-29 16:03:46 +0400

    edited exception mapper

--- src/main/java/ge/edu/freeuni/sdp/xo/rooms/service/MyExceptionMapper.java
+++ src/main/java/ge/edu/freeuni/sdp/xo/rooms/service/MyExceptionMapper.java
@@ -1,15 +1,14 @@
 package ge.edu.freeuni.sdp.xo.rooms.service;
 
-import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
 @Provider
 public class MyExceptionMapper  implements
-        ExceptionMapper<WebApplicationException> {
+        ExceptionMapper<Exception> {
     @Override
-    public Response toResponse(WebApplicationException ex) {
+    public Response toResponse(Exception ex) {
         return Response.status(500).entity(ex.getMessage()).type("text/plain")
                 .build();
     }
